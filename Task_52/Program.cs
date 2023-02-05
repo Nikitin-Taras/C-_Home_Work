bb656e6e413239fa2be4ef8bf00dd5abc6be419a
Tarik <nikitin-taras@yandex.ru>
2023-02-05 15:10:13 +0200

    add task 52

--- Task_52/Program.cs
+++ Task_52/Program.cs
@@ -5,3 +5,53 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int row = 3;
+int col = 3;
+
+double[,] arr = new double[row, col];
+
+FillArray(arr);
+PrintArr(arr);
+Console.Write($"Среднее арифметическое значение в столбцах = ");
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double avg = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        avg = (avg + arr[i, j]);
+    }
+    avg = avg / row;
+    Console.Write(avg + "; ");
+}
+Console.WriteLine();
+
+
+ 
+
+void FillArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArr(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            
+            Console.Write($"{arr[i, j], 4}");
+        }
+        Console.WriteLine();
+    }
+}
+ 
+
